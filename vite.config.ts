dee749aaa9fe0a42f18d0e4765b0d194834b98b0
jastrzt2 <tjastrzebski102@gmail.com>
2024-04-30 19:58:32 +0200

    test 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(async ({ mode }) => {
+  const env = await loadEnv(mode, process.cwd());
+  
+  return {
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  build: {
-    outDir: 'build',
-  }
+    build: {
+      outDir: 'build',
+    },
+    define: {
+      // Przekazujemy zmienne środowiskowe do aplikacji
+      'process.env': env
+    }
+  };
 });
