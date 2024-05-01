a7da3a8b6e1bc59e2c33130ea442d16de1c30bd3
jastrzt2 <tjastrzebski102@gmail.com>
2024-05-01 23:31:18 +0200

    server.js 2

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
-const history = require('express-history-api-fallback');
-
 const app = express();
-const root = path.join(__dirname, 'build');
 
-app.use(express.static(root));
-app.use(history('index.html', { root }));
+// Folder z plikami statycznymi
+const publicPath = path.join(__dirname, 'build');
+app.use(express.static(publicPath));
+
+// Obsługa wszystkich ścieżek
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
