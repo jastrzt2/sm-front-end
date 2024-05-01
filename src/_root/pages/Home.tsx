fee3c9b78dcd20d030685e76744561170a23c3d9
jastrzt2 <tjastrzebski102@gmail.com>
2024-05-01 21:23:36 +0200

    home fiexed for now

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,10 +1,25 @@
 import Loader from '@/components/shared/Loader';
 import PostCard from '@/components/shared/PostCard';
-import { useGetRecentPosts } from '@/lib/react-query/queriesAndMutations';
+import { useGetPosts } from '@/lib/react-query/queriesAndMutations';
 import { IPost } from '@/types';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export const Home = () => {
-  const { data: posts, isLoading: isPostLoading } = useGetRecentPosts();
+  const { ref, inView } = useInView();
+  const {
+    data: posts,
+    isLoading: isPostLoading,
+    fetchNextPage,
+    hasNextPage,
+  } = useGetPosts(); // Zakładam, że ten hook jest już skonfigurowany do paginacji
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
   return (
     <div className="flex flex-1">
       <div className="home-container">
@@ -16,20 +31,22 @@ export const Home = () => {
             <Loader />
           ) : (
             <ul className="flex flex-col flex-1 gap-9 w-full">
-              {posts?.map((post: IPost) => (
-                <li key={post.id} className="flex justify-center w-full">
+              {posts?.pages.map((page, index) => page.map((post: IPost) => (
+                <li key={`post-${index}-${post.id}`} className="flex justify-center w-full">
                   <PostCard post={post} />
                 </li>
-              ))}
+              )))}
             </ul>
-
-          )
-          }
+          )}
+          {hasNextPage && (
+            <div ref={ref} className="mt-10">
+              <Loader />
+            </div>
+          )}
         </div>
       </div>
     </div>
   )
 }
 
-
-export default Home
+export default Home;
